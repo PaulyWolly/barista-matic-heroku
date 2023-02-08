e0a86c6fe9a8d8ce0fec915629f61af6299b5401
Paul Welby <pwelby@gmail.com>
2023-02-08 07:57:52 -0800

    update eServer.js

--- eServer.js
+++ eServer.js
@@ -16,17 +16,17 @@ app.listen(Eport, () => {
 })
 
 // Json-Server setup
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+// const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-server.use(router)
+// server.use(middlewares)
+// server.use(router)
 
-const port = process.env.PORT || 3200
+// const port = process.env.PORT || 3200
 
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+// server.listen(port, () => {
+//     console.log(`JSON Server is running on port ${port}`)
+// })
